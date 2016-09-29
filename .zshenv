94758973664b7ad9fd18c260da6b24c95d6ca15a
nameko1 <nameko816.07@gmail.com>
2016-09-29 17:39:56 +0900

    update script

---
+++ .zshenv
@@ -0,0 +1,3 @@
+export RYU_HOME="/usr/local/lib/python2.7/dist-packages/ryu/app"
+export EDITOR="/usr/bin/vim"
+export LANG="en_US.UTF-8"
--- setup.sh
+++ setup.sh
@@ -1,8 +1,50 @@
 #!/bin/bash
 
-cp .vimrc ~/
-cp .zshrc ~/
-cp .tmux ~/
+# DOTFILES=('.vimrc' '.zshrc' '.zshenv' '.tmux')
 
-mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# for file in ${DOTFILES[*]}
+# do
+#     if [ ! -s ~/$file ];then
+#         cp $file ~/
+#     fi
+# done
+
+# while getopts f: o; do
+#     case $o in
+#         f)
+#             echo $OPTARG ;;
+#         /?) exit 1;;
+#     esac
+# done
+
+while [[ $# > 0 ]]; do
+    case $1 in
+        -b|--bundle) 
+            bundle=true;;
+        -t|--tmux)
+            tmux=true;;
+        -v| --vim)
+            vimrc=true;;
+        -z| --zsh)
+            zsh=true;;
+        *) echo "Unknown options:' $1"
+    esac
+    shift
+done
+
+if [ $tmux ];then
+    cp .tmux.conf ~/
+fi
+
+if [ $vimrc ];then
+    cp .vimrc ~/
+fi
+
+if [ $zsh ];then
+    cp .zshrc .zshenv ~/
+fi
+
+if [ $bundle ];then
+    mkdir -p ~/.vim/bundle
+    git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
