85b2876c9ed427751e375444e29e242e5d70fa52
なめこ <nameko@akimac23.kyoto-su.ac.jp>
2018-06-11 11:10:32 +0900

    [setup] オプションを廃止

--- setup.sh
+++ setup.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
 DOT_FILES=()
-HOME=$HOME
 CURRENT_DIR=$(cd $(dirname $0) && pwd)
 
-while getopts tvz-: o; do
-  case "$o" in
-    -)
-      case "${OPTARG}" in
-        all)
-          DOT_FILES=('.tmux.conf' '.vimrc' '.zshrc')
-          vi=true
-          zsh=true;;
-        home)
-          HOME=${BASH_ARGV}
-          if [ ! -e $HOME ]; then
-          echo "$HOME is not exist"; exit 1
-          fi
-          #cd $HOME > /dev/null 2>&1 || echo "$HOME is not exist"; exit 1
-          ;;
-        tmux)
-          DOT_FILES=('.tmux.conf' "${DOT_FILES[@]}")
-          ;;
-        vim)
-          DOT_FILES=('.vimrc' "${DOT_FILES[@]}")
-          vi=true;;
-      esac;;
-    t)
-      DOT_FILES=('.tmux.conf' "${DOT_FILES[@]}")
-      ;;
-    v)
-      DOT_FILES=('.vimrc' "${DOT_FILES[@]}")
-      vi=true;;
-  esac
-done
+DOT_FILES=('.tmux.conf' '.vimrc')
 
 for dotfile in "${DOT_FILES[@]}"
 do
@@ -43,12 +13,11 @@ do
   cp $CURRENT_DIR/$dotfile $HOME
 done
 
-if [ $vi ]; then
-  if [ ! -e $HOME/.vim/toml ]; then
-    mkdir -p $HOME/.vim/toml
-  fi
-  cp -f $CURRENT_DIR/dein.toml $HOME/.vim/toml/
+# vi setting
+if [ ! -e $HOME/.vim/toml ]; then
+  mkdir -p $HOME/.vim/toml
 fi
+cp -f $CURRENT_DIR/dein.toml $HOME/.vim/toml/
 
 # zsh setting
 if [ ! -e $HOME/.zshenv ]; then
