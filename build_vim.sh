2c2b9510bdf7600df60890286502adcccb145d23
なめこ <nameko@nameko.local>
2018-08-21 10:24:42 +0900

    update

--- build_vim.sh
+++ build_vim.sh
@@ -5,22 +5,33 @@ CURRENT_DIR=$(cd $(dirname $0) && pwd)
 # sudo apt remove -y --purge vim vim-runtime vim-common
 # sudo apt install -y git build-essential ncurses-dev lua5.2 lua5.2-dev
 
+# install vim source code
+git submodule update -i
+
 build_vim() {
-  cd $CURRENT_DIR/vim
+    cd $CURRENT_DIR/vim
 
-  ./configure \
-    --enable-luainterp=dynamic \
-    --enable-gpm \
-    --enable-fail-if-missing \
-    --with-features=huge \
-    --disable-selinux \
-    --enable-luainterp \
-    --enable-cscope \
-    --enable-fontset \
-    --enable-multibyte
+    ./configure \
+        --enable-luainterp=dynamic \
+        --enable-gpm \
+        --enable-fail-if-missing \
+        --with-features=huge \
+        --disable-selinux \
+        --enable-luainterp \
+        --enable-cscope \
+        --enable-fontset \
+        --enable-multibyte
 
-  make
-  sudo make install
+    make
+    sudo make install
+}
+
+install_package_centos() {
+    sudo yum install -y ncurses-devel lua-devel make gcc
+}
+
+install_package_ubuntu() {
+    sudo apt install -y git build-essential ncurses-dev lua5.2 lua5.2-dev
 }
 
 build_vim
